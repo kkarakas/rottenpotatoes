fc5b20bd3ed74aef694a600ca42859ee6fab8f83
Kutay Karakas <kk3528@columbia.edu>
2022-10-06 17:59:54 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,16 +14,16 @@ class MoviesController < ApplicationController
     @redirect = false
 
     if params.has_key?(:ratings) #param has it
-      puts "inside 17"
+      # puts "inside 17"
       @ratings_to_show = params[:ratings].keys
       session[:ratings] = params[:ratings]
       @hashed_ratings_to_show = Hash[@ratings_to_show.collect {|key| [key, '1']}]
     elsif ! params.has_key?(:ratings) && ! session.has_key?(:ratings) #both doesnt have it 
-      puts "inside 21"
+      # puts "inside 21"
       @ratings_to_show = @all_ratings
       @hashed_ratings_to_show = Hash[@ratings_to_show.collect {|key| [key, '1']}]
     else #param doesnt have it but session does
-      puts "inside 26"
+      # puts "inside 26"
       @redirect = true
       # puts session[:ratings] # TEMPS
       #this is stupid right now CHANGE IT
@@ -42,7 +42,7 @@ class MoviesController < ApplicationController
     @title_header = ""
     @release_date_header = ""
     if params[:sort_by] == "title" #sorting based on title
-      puts "inside 44"
+      # puts "inside 44"
       @title_header = "hilite bg-secondary"
       @movies = @movies.order(params[:sort_by])
       #update session
@@ -51,17 +51,17 @@ class MoviesController < ApplicationController
     
     # puts params[:sort_by]
     elsif params[:sort_by] == "release_date" #sorting based on date
-      puts "inside 53"
+      # puts "inside 53"
       @release_date_header = "hilite bg-secondary"
       @movies = @movies.order(params[:sort_by])
       #update session
       session[:sort_by] = params[:sort_by]
     
     elsif ! params.has_key?(:sort_by) && ! session.has_key?(:sort_by) #both doesnt have it 
-      puts "inside 60"
+      # puts "inside 60"
       
     else #not sorting???
-      puts "inside 64"
+      # puts "inside 64"
       @redirect = true
       params[:sort_by] = session[:sort_by] #EEEPOJELINHERE
       @release_date_header = "hilite bg-secondary"
@@ -71,7 +71,6 @@ class MoviesController < ApplicationController
     end
 
     if @redirect
-      puts "\n\n\n\nHEREEEEEEEEE\n\n\n\n"
       @redirect = false
       redirect_to movies_path( :sort_by => params[:sort_by], :ratings => params[:ratings])
     end
